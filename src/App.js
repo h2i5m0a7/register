0e4b1721beda7ba168f84f3961043c6fda5aa4ee
agrawalhimani@gmail.com <agrawalhimani146@gamil.com>
2023-11-04 18:10:55 +0545

    login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+
 
 const App = () => {
-  const[name, setname]=useState()
+  const[formData,SetFormData] =useState({
+    username:"",
+    address:"",
+    email:"",
+    password:"",
+
+  })
+  const handlechange=(e)=> {
+    SetFormData({
+      ...formData,[e.target.name]:e.target.value
+    })
+  }
+const handleSubmit=(e)=> {
+  e.preventDefault();
+  alert("register succesfully")
+}
+
+  
   return (
-    <div>
-      <h1>{name}</h1>
-      <input type="text" onChange={(e)=>setname(e.target.value)}/>
+   <div className="container">
+    <div className="register">
+      <form onSubmit={handleSubmit} >
+        <center><h1>Register</h1></center>
+        <input type="text" placeholder="Username" onChange={handlechange} required/>
+        <input type="text" placeholder=" Address"  onChange={handlechange}required />
+        <input type="email" placeholder="Email"onChange={handlechange} required/>
+        <input type="password" placeholder=" Password" onChange={handlechange} required />
+        <button type="submit"> Submit </button>
+      </form>
     </div>
+   </div>
+
   )
 }
 
