8405afddf827422229e62cdb7aab56c590bcf71a
agrawalhimani@gmail.com <agrawalhimani146@gamil.com>
2023-11-07 18:03:25 +0545

    message

--- src/App.js
+++ src/App.js
@@ -1,42 +1,93 @@
-import React from 'react'
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import axios from 'axios';
 
+function App() {
+  const [formData, setFormData] = useState({
+    
+    username: '',
+    address:'',
+    email: '',
+    password:'',
+   
+   
+  });
 
-const App = () => {
-  const[formData,SetFormData] =useState({
-    username:"",
-    address:"",
-    email:"",
-    password:"",
+  const handleChange = (e) => {
+    
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
-  })
-  const handlechange=(e)=> {
-    SetFormData({
-      ...formData,[e.target.name]:e.target.value
-    })
-  }
-const handleSubmit=(e)=> {
-  e.preventDefault();
-  alert("register succesfully")
-}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  
+    try {
+      const response = await axios.post('http://192.168.1.66:8800/app/login', formData);
+
+     
+      console.log('Response:', response.data);
+    } catch (error) {
+      
+      console.error('Error:', error);
+    }
+  };
+
   return (
-   <div className="container">
-    <div className="register">
-      <form onSubmit={handleSubmit} >
-        <center><h1>Register</h1></center>
-        <input type="text" placeholder="Username" onChange={handlechange} required/>
-        <input type="text" placeholder=" Address"  onChange={handlechange}required />
-        <input type="email" placeholder="Email"onChange={handlechange} required/>
-        <input type="password" placeholder=" Password" onChange={handlechange} required />
-        <button type="submit"> Submit </button>
+    <div>
+      <h2>POST Request Example</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Name:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="address">Address:</label>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+
+      <div>
+      <label htmlFor="password">password:</label>
+      <input
+        type="text"
+        id="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+    </div>
+
+   
+
+      
+        <div>
+          <button type="submit">Submit</button>
+        </div>
       </form>
     </div>
-   </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
